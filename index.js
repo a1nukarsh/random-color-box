892c177c9205cf96f2d60cb079a3d8c3d08ec4df
Anukarsh <anukarshr@gmail.com>
2024-06-28 01:10:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("hello dear")
+
+let boxes = document.getElementsByClassName("box")
+console.log(boxes)
+
+function getRandomColor(){
+    let v1=233
+    let v2=124
+    let v3=111
+    return `rgb(${v1}, ${v2}, ${v3})`
+}
+
+Array.from(boxes).forEach(e=>{
+    e.style.backgroundColor = getRandomColor()  
+})
