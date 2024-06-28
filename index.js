3322ad57a7bdbe8532bfd7446514fcdce601921c
Anukarsh <anukarshr@gmail.com>
2024-06-28 16:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ let boxes = document.getElementsByClassName("box")
 console.log(boxes)
 
 function getRandomColor(){
-    let v1=233
-    let v2=124
-    let v3=111
+    let v1= 0+Math.random()*(255-0)
+    let v2= 0+Math.random()*(255-0)
+    let v3= 0+Math.random()*(255-0)
     return `rgb(${v1}, ${v2}, ${v3})`
 }
 
 Array.from(boxes).forEach(e=>{
     e.style.backgroundColor = getRandomColor()  
+    e.style.color = getRandomColor()
 })
