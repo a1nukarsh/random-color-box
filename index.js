74acdffaba54c9ffdf20c20a09aabe5eae9caf69
Anukarsh <anukarshr@gmail.com>
2024-06-28 16:48:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let boxes = document.getElementsByClassName("box")
 console.log(boxes)
 
 function getRandomColor(){
-    let v1= 0+Math.random()*(255-0)
-    let v2= 0+Math.random()*(255-0)
-    let v3= 0+Math.random()*(255-0)
+    let v1= Math.ceil(0+Math.random()*(255-0))
+    let v2= Math.ceil(0+Math.random()*(255-0))
+    let v3= Math.ceil(0+Math.random()*(255-0))
     return `rgb(${v1}, ${v2}, ${v3})`
 }
 
